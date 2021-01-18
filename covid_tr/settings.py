7915001d239233adbc0483b2ea546d45678bfa5e
Pratik Sasmal <pratiksasmaljedi@gmail.com>
2021-01-18 18:22:47 +0530

    separating css file from html

--- covid_tr/settings.py
+++ covid_tr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'covid',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = (
-#   os.path.join(BASE_DIR,''),
-#)
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR,'static')
+]
